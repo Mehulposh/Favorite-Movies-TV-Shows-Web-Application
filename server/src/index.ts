952444fd52787eee438a14d4f5ab3a7e9748685a
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-10-27 22:08:24 +0530

    added prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 
 const cors = require( 'cors');
 import express, { Request, Response } from 'express';
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from TypeScript backend!' });
